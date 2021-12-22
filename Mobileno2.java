818421c09d4ca79d358ab4098c9df2ce54a8312b
ullaskumbati <ullasbk.xworkz@gmail.com>
2021-12-22 23:57:37 +0530

    arrayoutofbound for printing numbers of a group

---
+++ Mobileno2.java
@@ -0,0 +1,13 @@
+class Mobileno2 {
+	public static void main (String [] ar){
+		String [] numbers= {"7892564006","9591931134","9902436835","7975780647","7338144269","8762902478"};
+		
+		for(int index=0; index < numbers.length;index =index-3){
+		System.out.println(numbers[index]);
+		}
+
+
+	}
+
+
+}
