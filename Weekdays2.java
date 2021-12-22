011ed974f4df45a9b27f0bf54d1ced44644ba99b
ullaskumbati <ullasbk.xworkz@gmail.com>
2021-12-22 23:52:52 +0530

    weekdays printing with loop but arrayoutofboundexception

---
+++ Weekdays2.java
@@ -0,0 +1,13 @@
+class Weekdays2 {
+	public static void main (String [] ar){
+		String [] days= {"Sunday","monday","tuesdat","wednesday","thursday","friday","saturday"};
+		
+		for(int index=0; index < days.length;index =index-1){
+		System.out.println(days[index]);
+		}
+
+
+	}
+
+
+}
