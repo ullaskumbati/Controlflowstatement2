e343efa17d1d8f52dd96a29961bb447a9466ce1d
ullaskumbati <ullasbk.xworkz@gmail.com>
2021-12-22 23:55:09 +0530

    month arrayoutofboundexception

---
+++ Month2.java
@@ -0,0 +1,13 @@
+class Month2 {
+	public static void main (String [] ar){
+		String [] months= {"January","February","March","April","May","June","July","August","September","October","November","December"};
+		
+		for(int index=0; index < months.length;index =index-2){
+		System.out.println(months[index]);
+		}
+
+
+	}
+
+
+}
