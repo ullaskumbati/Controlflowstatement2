bf6ea9762af8f7186736d7f12da9d299de5fcf13
ullaskumbati <ullasbk.xworkz@gmail.com>
2021-12-22 23:58:48 +0530

    printed days of a week in a same line

---
+++ Concatinate.java
@@ -0,0 +1,13 @@
+class Concatinate {
+	public static void main (String [] ar){
+		String [] days= {"Sunday","monday","tuesdat","wednesday","thursday","friday","saturday"};
+		
+		
+		System.out.println(days[0]+""+days[1]+""+days[2]+""+days[3]+""+days[4]+""+days[5]+""+days[6]);
+		
+
+
+	}
+
+
+}
